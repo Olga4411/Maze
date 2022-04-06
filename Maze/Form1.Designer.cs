872aea75666925b0698afa2a3b8ed6c53e6613b4
Новичковская Ольга <miss4411@yandex.ru>
2022-04-07 06:07:07 +0700

    Add project files.

---
+++ Maze/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace Maze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.finishlabel = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Location = new System.Drawing.Point(63, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label2.Location = new System.Drawing.Point(3, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "\r\n";
+            this.label2.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label3.Location = new System.Drawing.Point(163, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 189);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "\r\n";
+            this.label3.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label4.Location = new System.Drawing.Point(106, 1);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "\r\n";
+            this.label4.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(66, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 249);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "\r\n";
+            this.label1.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label5.Location = new System.Drawing.Point(226, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 29);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "\r\n";
+            this.label5.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label6.Location = new System.Drawing.Point(66, 381);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 44);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "\r\n";
+            this.label6.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label7.Location = new System.Drawing.Point(319, 118);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 249);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "\r\n";
+            this.label7.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label8.Location = new System.Drawing.Point(405, 118);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 249);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "\r\n";
+            this.label8.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.label9.Location = new System.Drawing.Point(240, 87);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(232, 31);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "\r\n";
+            this.label9.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            // 
+            // finishlabel
+            // 
+            this.finishlabel.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.finishlabel.Location = new System.Drawing.Point(475, 427);
+            this.finishlabel.Name = "finishlabel";
+            this.finishlabel.Size = new System.Drawing.Size(100, 66);
+            this.finishlabel.TabIndex = 10;
+            this.finishlabel.Text = "finish";
+            this.finishlabel.MouseEnter += new System.EventHandler(this.finishlabel_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Aqua;
+            this.ClientSize = new System.Drawing.Size(566, 491);
+            this.Controls.Add(this.finishlabel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Maze";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label finishlabel;
+    }
+}
+
---
+++ Maze/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maze
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MoveToStart();
+        }
+
+        //private void label10_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void finishlabel_MouseEnter(object sender, EventArgs e)
+        {
+            MessageBox.Show("Уровень пройден");
+            Form Form2 = new Form();
+            Form2.Show();
+          //  Hide();
+
+        }
+        private void MoveToStart()
+        {
+            Point startPoint = panel1.Location;
+            startPoint.Offset(10, 10);
+            Cursor.Position = PointToScreen(startPoint);
+        }
+
+        private void panel1_MouseEnter(object sender, EventArgs e)
+        {
+            MoveToStart();
+        }
+    }
+}
